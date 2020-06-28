7adb04d9b516f3a05f82a7abc4b28571d57938f3
Leslee <leelovesc@gmail.com>
2020-06-29 00:11:12 +0800

    1. 增加模型

--- modeling.py
+++ modeling.py
@@ -79,7 +79,7 @@ class BertModel(object):
 
 
     def __init__(self,
-                 config,is_traning,
+                 config,is_training,
                  input_ids, input_mask=None,
                  token_type_ids=None,use_one_hot_embeddings=False,
                  scope=None):
@@ -94,7 +94,7 @@ class BertModel(object):
         :param scope: 参数域
         '''
         config = copy.deepcopy(config)
-        if not is_traning:
+        if not is_training:
             config.hidden_dropout_prob = 0.0
             config.attention_probs_dropout_prob = 0.0
 
@@ -134,8 +134,41 @@ class BertModel(object):
                 attention_mask = create_attention_mask_from_input_mask(
                     input_ids, input_mask)
 
+                # `sequence_output` shape = [batch_size, seq_length, hidden_size].
+                self.all_encoder_layers = transformer_model(
+                    input_tensor=self.embedding_output, attention_mask=attention_mask,
+                    hidden_size=config.hidden_size, num_hidden_layers=config.num_hidden_layers,
+                    num_attention_heads=config.num_attention_heads,intermediate_size=config.intermediate_size,
+                    intermediate_act_fn=get_activation(config.hidden_act), hidden_dropout_prob=config.hidden_dropout_prob,
+                    attention_probs_dropout_prob=config.attention_probs_dropout_prob,initializer_range=config.initializer_range,
+                    do_return_all_layers=True)
 
+            self.sequence_output = self.all_encoder_layers[-1]
+            # pooler 将encoder的sequence tensor 从[batch_size, seq_len, hidden_size] 转换为'
+            # [batch_size,hidden_size]. 对一些句子对分类任务比较友好。
+            with tf.variable_scope("pooler"):
+                #  取句子的第一个token的隐藏层状态作为语义表示
+                first_token_tensor = tf.squeeze(self.sequence_output[:, 0:1, :], axis=1)
+                self.pooled_output = tf.layers.dense(
+                    first_token_tensor, config.hidden_size, activation=tf.tanh,
+                    kernel_initializer=create_initializer(config.initializer_range))
 
+    def get_pooled_output(self):
+        return self.pooled_output
+
+    def get_sequence_output(self):
+        # 获取encoder的最后一个隐藏层。
+        # 返回size： [batch_size, seq_length, hidden_size]
+        return self.sequence_output
+
+    def get_all_encoder_layers(self):
+        return self.all_encoder_layers
+
+    def get_embedding_output(self):
+        return self.embedding_output
+
+    def get_embedding_table(self):
+        return self.embedding_table
 
 
 
@@ -252,8 +285,19 @@ def create_attention_mask_from_input_mask(from_tensor,to_mask):
     :return:
         float Tensor of shape [batch_size, from_seq_length, to_seq_length].
     '''
+    from_shape = get_shape_list(from_tensor,expected_rank=[2, 3])
+    batch_size = from_shape[0]
+    from_seq_length = from_shape[1]
 
+    to_shape = get_shape_list(to_mask,expected_rank=2)
+    to_seq_length = to_shape[1]
+    # tf.cast：改变tensor的数据类型
+    to_mask = tf.cast(tf.reshape(to_mask, [batch_size,1,to_seq_length]), tf.float32)
 
+    broadcast_ones = tf.ones(shape=[batch_size, from_seq_length,1], dtype=tf.float32)
+
+    mask = broadcast_ones * to_mask
+    return mask
 
 
 
@@ -304,16 +348,296 @@ def layer_norm_and_dropout(input_tensor,dropout_prob,name=None):
     return output_tensor
 
 
+def gelu(x):
+
+    cdf = 0.5 * (1.0 + tf.tanh(
+        (np.sqrt(2 / np.pi) * (x + 0.044715 * tf.pow(x, 3)))))
+    return x * cdf
+
+def get_activation(activation_string):
+    # 将指定的一个string 转换为 对应的激活函数
+    if not isinstance(activation_string,six.string_types):
+        return activation_string
+    if not activation_string:
+        return None
+
+    act = activation_string.lower()
+    if act == "linear":
+        return None
+    elif act == 'relu':
+        return tf.nn.relu
+    elif act == 'gelu':
+        return gelu
+    elif act == 'tanh':
+        return tf.tanh
+    else:
+        raise ValueError("Unsupported activation: %s" % act)
+
+def get_assignment_map_from_checkpoint(tvars, init_checkpoint):
+
+    initialized_variable_names = {}
+    name_to_variable = collections.OrderedDict()
+    for var in tvars:
+        name = var.name
+        m = re.match("^(.*):\\d+$", name)
+        if m is not None:
+            name = m.group(1)
+        name_to_variable[name] = var
+
+    init_vars = tf.train.list_variables(init_checkpoint)
+
+    assignment_map = collections.OrderedDict()
+    for x in init_vars:
+        (name, var) = (x[0], x[1])
+        if name not in name_to_variable:
+            continue
+        assignment_map[name] = name
+        initialized_variable_names[name] = 1
+        initialized_variable_names[name + ":0"] = 1
+
+    return (assignment_map, initialized_variable_names)
 
 
 
+def attention_layer(from_tensor,
+                    to_tensor,
+                    attention_mask=None,
+                    num_attention_heads =1,
+                    size_pre_head=512,
+                    query_act =None,
+                    key_act = None,
+                    value_act = None,
+                    attention_probs_dropout_prob=0.0,
+                    initializer_range=0.02,
+                    do_return_2d_tensor=False,
+                    batch_size=None,
+                    from_seq_length=None,
+                    to_seq_length=None):
+    '''
+    如果from_tensor和to_tensor是相同的，name就是self-attention. 每一个时间步，from_tensor都会和to_tensor进行关联计算，
+    即计算 点积，得到一个向量。
+
+    这里首先把 "from_tensor"复制为 query tensor，把"to_tensor"转换为 key tensor 和 value tensor。每个tensor都是
+    [batch_size,seq_len, size_per_head].然后query和key进行点积归一化（这里用softmax作为概率结果），最后value将这些概率乘起来
+    作为概率加权，然后返回结果。
+    :param from_tensor: [batch_size, from_seq_length, from_width]
+    :param to_tensor: [batch_size, to_seq_length, to_width]
+    :param attention_mask:[batch_size, from_seq_length, to_seq_length],值是1或0，对为0的
+    位置设置为-infinity,对为1的位置设置不变。
+    :param num_attention_heads: 注意力头的数量
+    :param size_pre_head: 每个注意力 head的size
+    :param query_act: query的激活函数
+    :param key_act: key的激活函数
+    :param value_act: value的激活函数
+    :param attention_probs_dropout_prob: attention概率的dropout概率
+    :param initializer_range: 权重初始化范围
+    :param do_return_2d_tensor: 如果为True，返回[batch_size * from_seq_length,num_attention_heads*size_per_head]
+    如果是False，返回[batch_size, from_seq_length,num_attention_heads*size_per_heads]
+    :param batch_size:如果输入是2D的，则batch_size是3D版本中的batch——size
+    :param from_seq_length:from_tensor 的seq len
+    :param to_seq_length:
+    :return:
+    float Tensor of shape [batch_size, from_seq_length,
+      num_attention_heads * size_per_head]. (If `do_return_2d_tensor` is
+      true, this will be of shape [batch_size * from_seq_length,
+      num_attention_heads * size_per_head]).
+    '''
+    def transpose_for_scores(input_tensor, batch_size,
+                             num_attention_heads,seq_length, width):
+        output_tensor = tf.reshape(
+            input_tensor, [batch_size,seq_length,num_attention_heads,width])
+        output_tensor = tf.transpose(output_tensor,[0,2,1,3])
+        return output_tensor
+
+    from_shape = get_shape_list(from_tensor, expected_rank=[2, 3])
+    to_shape = get_shape_list(to_tensor, expected_rank=[2 ,3])
+    if len(from_shape) != len(to_shape):
+        raise ValueError(
+            "The rank of `from_tensor` must match the rank of `to_tensor`.")
+
+    if len(from_shape) == 3:
+        batch_size = from_shape[0]
+        from_seq_length = from_shape[1]
+        to_seq_length = to_shape[1]
+    elif len(from_shape) == 2:
+        if (batch_size is None or from_seq_length is None or to_seq_length is None):
+            raise ValueError(
+                "When passing in rank 2 tensors to attention_layer, the values "
+                "for `batch_size`, `from_seq_length`, and `to_seq_length` "
+                "must all be specified.")
+
+    # 名词解释
+    # Scalar dimensions referenced here:
+    #   B = batch size (number of sequences)
+    #   F = `from_tensor` sequence length
+    #   T = `to_tensor` sequence length
+    #   N = `num_attention_heads`
+    #   H = `size_per_head`
+    from_tensor_2d = reshape_to_matrix(from_tensor)
+    to_tensor_2d = reshape_to_matrix(to_tensor)
+    # query_layer = [B*F,N*H]
+    query_layer = tf.layers.dense(
+        from_tensor_2d,
+        num_attention_heads * size_pre_head,
+        activation= query_act,
+        name="query",
+        kernel_initializer=create_initializer(initializer_range))
+    # key_layer = [B*T, N*H]
+    key_layer = tf.layers.dense(
+        to_tensor_2d, num_attention_heads * size_pre_head,
+        activation=key_act, name="key", kernel_initializer=create_initializer(initializer_range))
+
+    # value_layer = [B*T, N*H]
+    value_layer = tf.layers.dense(
+        to_tensor_2d, num_attention_heads * size_pre_head,
+        activation=key_act, name="value", kernel_initializer=create_initializer(initializer_range))
+
+    # query_layer = [B, N,F,H]
+    query_layer = transpose_for_scores(query_layer,batch_size,num_attention_heads,
+                                       from_seq_length,size_pre_head)
+    # key_layer = [B,N,T,H]
+    key_layer = transpose_for_scores(key_layer, batch_size, num_attention_heads,
+                                     to_seq_length,size_pre_head)
+    # 将query 和 key进行点乘，获得注意力分数
+    # attention_scores = [B,N,F,T]
+    attention_scores = tf.matmul(query_layer, key_layer, transpose_b=True)
+    attention_scores = tf.multiply(attention_scores, 1.0/math.sqrt(float(size_pre_head))) # 乘以一个分数
+
+    if attention_mask is not None:
+        # attention_mask = [B,1,F, T]
+        attention_mask = tf.expand_dims(attention_mask,axis=[1])
+        # 创建一个函数，对于参与注意力计算的参数，位置时0.0，对于mask掉的position，取 -10000.0
+        adder = (1.0 - tf.cast(attention_mask,tf.float32)) * -10000.0
+        # 在softmax前将分数与adder相加，实际上等同于删除所有的masked。
+        attention_scores += adder
+
+    # 取softmax; [B,N,F,T]
+    attention_probs = tf.nn.softmax(attention_scores)
+    attention_probs = dropout(attention_probs, attention_probs_dropout_prob)
+
+    # 处理value -->value_layer = [B,T,N,H]
+    value_layer = tf.reshape(value_layer,
+                             [batch_size,to_seq_length,num_attention_heads,size_pre_head])
+    # 转置
+    value_layer = tf.transpose(value_layer, [0,2,1,3])
+    # 乘以上下文注意力权重
+    # context_layer = [B,N,F,H]
+    context_layer = tf.matmul(attention_probs,value_layer)
+
+    # 再转置
+    context_layer = tf.transpose(context_layer,[0,2,1,3])
+
+    if do_return_2d_tensor:
+        # context_layer = [B*F, N*H]
+        context_layer = tf.reshape(
+            context_layer, [batch_size*from_seq_length, num_attention_heads*size_pre_head])
+    else:
+        # 拆成 [B,F,N*H]
+        context_layer = tf.reshape(context_layer,
+                                   [batch_size,from_seq_length,num_attention_heads*size_pre_head])
+    return context_layer
+
+def transformer_model(input_tensor,
+                      attention_mask=None,
+                      hidden_size=768,
+                      num_hidden_layers=12,
+                      num_attention_heads=12,
+                      intermediate_size=3072,
+                      intermediate_act_fn=gelu,
+                      hidden_dropout_prob = 0.1,
+                      attention_probs_dropout_prob=0.1,
+                      initializer_range = 0.02,
+                      do_return_all_layers=False
+                      ):
+    '''
+
+    :param input_tensor: tensor。 Shape=[batch_size, seq_length, hidden_size]
+    :param attention_mask: [batch_size, seq_len, seq_len]，当该位置为1时，表名需要注意力关注，否则为0时不需要。
+    :param hidden_size: Transformer的隐藏层尺寸
+    :param num_hidden_layers: 隐藏层数量
+    :param num_attention_heads: 注意力头的数量
+    :param intermediate_size: 前馈神经网络的size
+    :param intermediate_act_fn: 应用于前馈输出层或者中间层的非线性激活函数
+    :param hidden_dropout_prob: 隐藏层的Dropout
+    :param attention_probs_dropout_prob: attention的dropout概率
+    :param initializer_range:初始化的概率范围
+    :param do_return_all_layers: 决定是否所有层都返回结果，还是只在最后一层返回结果
+    :return:
+    float Tensor of shape [batch_size, seq_length, hidden_size], the final
+    hidden layer of the Transformer.
+    '''
 
 
+    if hidden_size % num_attention_heads != 0:
+        raise ValueError(
+            "The hidden size (%d) is not a multiple of the number of attention "
+            "heads (%d)" % (hidden_size, num_attention_heads))
 
+    attention_head_size = int(hidden_size / num_attention_heads)
+    input_shape = get_shape_list(input_tensor, expected_rank=3)
+    batch_size = input_shape[0]
+    seq_length = input_shape[1]
+    input_width = input_shape[2]
 
+    # Transformer需要在所有的层上进行残差和加和运算，所以输入必须跟隐藏层size相同。
+    if input_width != hidden_size:
+        raise ValueError("The width of the input tensor (%d) != hidden size (%d)" %
+                         (input_width, hidden_size))
+    # 这里reshape成2D，因为使用TPU运算的时候不方便来回切换shape，但是对GPU不存在这样的性能差
+    prev_output = reshape_to_matrix(input_tensor)
 
+    all_layer_outputs = []
+    for layer_idx in range(num_hidden_layers):
+        with tf.variable_scope("layer_%d" % layer_idx):
+            layer_input = prev_output
 
+            with tf.variable_scope("attention"):
+                attention_heads = []
+                with tf.variable_scope("self"):
+                    attention_head = attention_layer(
+                        from_tensor= layer_input, to_tensor=layer_input,attention_mask=attention_mask,
+                        num_attention_heads=num_attention_heads, size_pre_head=attention_head_size,
+                        attention_probs_dropout_prob = attention_probs_dropout_prob,
+                        initializer_range=initializer_range,do_return_2d_tensor=True,batch_size=batch_size,
+                        from_seq_length=seq_length, to_seq_length=seq_length)
+                    attention_heads.append(attention_head)
 
+                attention_output = None
+                if len(attention_heads) == 1:
+                    attention_output = attention_heads[0]
+                else:
+                    # 将所有的sequences concatenate起来作为输出。
+                    attention_output = tf.concat(attention_heads, axis=-1)
+
+                # 线性投影层和残差网络
+                with tf.variable_scope("output"):
+                    attention_output = tf.layers.dense(
+                        attention_output,hidden_size,kernel_initializer=create_initializer(initializer_range))
+                    attention_output = dropout(attention_output, hidden_dropout_prob)
+                    attention_output = layer_norm(attention_output + layer_input)
+            # 该激活函数只应用于intermediate这个隐藏层
+            with tf.variable_scope("intermediate"):
+                intermediate_output = tf.layers.dense(
+                    attention_output,intermediate_size,
+                    activation=intermediate_act_fn, kernel_initializer=create_initializer(initializer_range))
+            # 向下投影回 ‘hidden_size’，并且添加残差
+            with tf.variable_scope("output"):
+                layer_output = tf.layers.dense(
+                    intermediate_output, hidden_size,kernel_initializer=create_initializer(initializer_range))
+                layer_output = dropout(layer_output,hidden_dropout_prob)
+                layer_output = layer_norm(layer_output + attention_output)
+                prev_output = layer_output
+                all_layer_outputs.append(layer_output)
+
+    if do_return_all_layers:
+        final_outputs = []
+        for layer_output in all_layer_outputs:
+            final_output = reshape_to_matrix(layer_output,input_shape)
+            final_outputs.append(final_output)
+        return final_outputs
+    else:
+        final_output = reshape_to_matrix(prev_output, input_shape)
+        return final_output
 
 
 
@@ -331,9 +655,45 @@ def get_shape_list(tensor, expected_rank=None,name=None):
         name = tensor.name
 
     if expected_rank is not None:
-        assert
+        assert_rank(tensor,expected_rank, name)
 
+    shape = tensor.shape.as_list()
 
+    non_static_indexes = []
+    for (index, dim) in enumerate(shape):
+        if dim is None:
+            non_static_indexes.append(index)
+
+    if not non_static_indexes:
+        return shape
+
+    dyn_shape = tf.shape(tensor)
+    for index in non_static_indexes:
+        shape[index] = dyn_shape[index]
+    return shape
+
+def reshape_to_matrix(input_tensor):
+    # 将大于二维的 tensor 转为 2维的 Tensor
+    ndims = input_tensor.shape.ndims
+    if ndims < 2:
+        raise ValueError("Input tensor must have at least rank 2. Shape = %s" %
+                         (input_tensor.shape))
+    if ndims == 2:
+        return input_tensor
+
+    width = input_tensor.shape[-1]
+    output_tensor = tf.reshape(input_tensor,[-1,width])
+    return output_tensor
+
+def reshape_from_matrix(output_tensor, orig_shape_list):
+    if len(orig_shape_list) == 2:
+        return output_tensor
+
+    output_shape = get_shape_list(output_tensor)
+    orig_dims = orig_shape_list[0:-1]
+    width = output_shape[-1]
+
+    return tf.reshape(output_tensor, orig_dims + [width])
 
 def assert_rank(tensor, expected_rank, name=None):
 
